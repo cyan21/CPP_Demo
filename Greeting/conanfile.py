cc9de5a1a6de8933f2a00dbb7acd09508b40b561
Yann Chaysinh <yannc@jfrog.com>
2018-08-07 19:45:16 +0000

    added conanfile.py to create greeting package

---
+++ Greeting/conanfile.py
@@ -0,0 +1,46 @@
+from conans import ConanFile, CMake, tools
+
+
+class GreetingConan(ConanFile):
+    name = "greeting"
+    version = "0.1"
+    license = "Apache-2.0"
+    url = "https://github.com/cyan21/CPP_Demo.git"
+    description = "greeting package"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/cyan21/CPP_Demo.git")
+        self.run("cd Greeting && git checkout cmake")
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        tools.replace_in_file("Greeting/CMakeLists.txt", "PROJECT(Greeting)",
+                              '''PROJECT(Greeting)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="Greeting")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src=".")
+        self.copy("*greet.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["greet"]
+
